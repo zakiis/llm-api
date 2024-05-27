290b269b7b16fb6140243eebfe42cfaf23d418fc
liuzhenghua-jk <liuzhenghua-jk@360shuke.com>
2024-05-27 14:46:46 +0800

    llm api

---
+++ script/qwen1_5_chat_stream.py
@@ -0,0 +1,27 @@
+import os
+from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
+from threading import Thread
+device = "cuda"  # the device to load the model onto
+
+checkpoint_path = os.getenv('MODELSCOPE_CACHE') + "/qwen/Qwen1___5-7B-Chat"
+model = AutoModelForCausalLM.from_pretrained(
+    "Qwen/Qwen1.5-7B-Chat",
+    torch_dtype="auto",
+    device_map="auto"
+)
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen1.5-7B-Chat")
+
+prompt = "Give me a short introduction to large language model."
+messages = [
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": prompt}
+]
+input_ids = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors='pt').to(device)
+streamer = TextIteratorStreamer(tokenizer=tokenizer, skip_prompt=True, timeout=60.0, skip_special_tokens=True)
+
+generation_kwargs = dict(input_ids=input_ids, streamer=streamer, max_new_tokens=512)
+thread = Thread(target=model.generate, kwargs=generation_kwargs)
+thread.start()
+
+for new_text in streamer:
+    print(new_text)
